d7c79f7ff8e2c1b4f894527fb62425827640b7e6
Ritik <ritikpal227@gmail>
2026-07-18 16:55:51 +0530

    Update Admin

--- server/routes/newsRoutes.js
+++ server/routes/newsRoutes.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const NewsController = require('../controllers/newsController');
-const auth = require('../middleware/auth');
-
+const {authenticate} = require('../middleware/auth'); 
 // Public routes
 router.get('/', NewsController.getAll);
 router.get('/latest', NewsController.getLatest);
@@ -12,8 +11,8 @@ router.get('/category/:category', NewsController.getByCategory);
 router.get('/:id', NewsController.getById);
 
 // Protected routes (admin only)
-router.post('/',  NewsController.create);
-router.put('/:id',  NewsController.update);
-router.delete('/:id', auth, NewsController.delete);
+router.post('/',  authenticate, NewsController.create);
+router.put('/:id', authenticate, NewsController.update);
+router.delete('/:id', authenticate, NewsController.delete);
 
 module.exports = router;

24361a25546a6822b8cafeb8ccf8e70a32a1185d
Ritik <ritikpal227@gmail>
2026-07-19 16:54:52 +0530

    Update Admin

--- server/app.js
+++ server/app.js
@@ -1,28 +1,40 @@
 // server/app.js
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 const allowedOrigins = [
-  'http://localhost:5173',
+  "http://localhost:5173",
   process.env.CLIENT_URL, // set this in Vercel env vars to your prod domain
 ].filter(Boolean);
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "https://real-estate-sand-five.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  }),
+);
+
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     credentials: true,
+//   }),
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-const newsRoutes = require('./routes/newsRoutes');
-app.use('/api/news', newsRoutes);
+const newsRoutes = require("./routes/newsRoutes");
+app.use("/api/news", newsRoutes);
 
-const authRoutes = require('./routes/auth.routes');
-app.use('/api/auth', authRoutes);
+const authRoutes = require("./routes/auth.routes");
+app.use("/api/auth", authRoutes);
 
 // app.get('/api/health', (req, res) => {
 //   res.json({ status: 'OK', message: 'Server is running' });
@@ -32,8 +44,8 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
     success: false,
-    message: 'Something went wrong!',
-    error: process.env.NODE_ENV === 'development' ? err.message : undefined
+    message: "Something went wrong!",
+    error: process.env.NODE_ENV === "development" ? err.message : undefined,
   });
 });
 

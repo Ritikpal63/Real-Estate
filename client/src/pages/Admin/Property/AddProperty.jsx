5a8251cf196462567d4d247ecd064f3b697eaa4c
Ritik <ritikpal227@gmail>
2026-07-20 12:28:14 +0530

    Image Uloader Update

--- client/src/pages/Admin/Property/AddProperty.jsx
+++ client/src/pages/Admin/Property/AddProperty.jsx
@@ -47,7 +47,7 @@ export default function AddProperty() {
           className="w-full bg-gray-100 rounded-xl mb-3 px-4 py-3 outline-none"
         />
 
-        // pehle: <div className="grid grid-cols-2 gap-4">
+       <div className="grid grid-cols-2 gap-4">
 <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
   <input type="number" placeholder="Bedrooms" className="bg-gray-100 rounded-xl px-4 py-3 w-full" />
   <input type="number" placeholder="Bathrooms" className="bg-gray-100 rounded-xl px-4 py-3 w-full" />

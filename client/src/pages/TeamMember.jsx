7fe41c097017c12ea6b4577ac7d00008cf533d27
Ritik <ritikpal227@gmail>
2026-07-20 17:49:43 +0530

    Push main

--- client/src/pages/TeamMember.jsx
+++ client/src/pages/TeamMember.jsx
@@ -1,28 +1,28 @@
-import React, {useState, useEffect } from 'react'
-import axiosInstance from '../utils/axiosConfig'
+import React, { useState, useEffect } from "react";
+import axiosInstance from "../utils/axiosConfig";
 
 const TeamMember = () => {
-      const [team, setTeam] = useState([]);
-      const [loading, setLoading] = useState(true);
-      const [error, setError] = useState(null);
-    const getNews = async () => {
+  const [team, setTeam] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const getNews = async () => {
     try {
       setLoading(true);
       setError(null);
       const res = await axiosInstance.get("/team");
-      const resData = res.data
+      const resData = res.data;
       if (res.data.success) {
         setTeam(res.data.data);
       } else {
         setError(res.data.message || "Failed to fetch team");
       }
 
-      console.log("Team: ", team)
+      console.log("Team: ", team);
       console.log("Team Data: ", resData);
     } catch (err) {
       setError(
         err.response?.data?.message ||
-          "Cannot connect to server. Please check if backend is running."
+          "Cannot connect to server. Please check if backend is running.",
       );
     } finally {
       setLoading(false);
@@ -31,10 +31,16 @@ const TeamMember = () => {
 
   useEffect(() => {
     getNews();
-  },[]);
+  }, []);
   return (
-    <div></div>
-  )
-}
+    <div>
+      {team?.map((item) => {
+        return <div key={item.id}>
+            <h4>{item.name}</h4>
+        </div>;
+      })}
+    </div>
+  );
+};
 
 export default TeamMember;

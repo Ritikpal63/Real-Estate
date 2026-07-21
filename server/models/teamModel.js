def292268ada546e421f6794a3488ac5f484553a
Ritik <ritikpal227@gmail>
2026-07-21 11:24:13 +0530

    Update Admin

--- server/models/teamModel.js
+++ server/models/teamModel.js
@@ -1,9 +1,9 @@
 const pool = require("../config/database");
 
-class TeamModel{
-    static async getAllTeam(){
-        const [rows] = await pool.query("SELECT * FROM team");
-        return rows;
-    }
+class TeamModel {
+  static async getAllTeam({limit, offset}) {
+    const [rows] = await pool.query("SELECT * FROM team LIMIT ? OFFSET ?",[limit, offset]);
+    return rows;
+  }
 }
 module.exports = TeamModel;

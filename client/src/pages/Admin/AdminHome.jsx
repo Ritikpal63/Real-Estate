72388c8e6c2fae74e138ea3cf8c058bca61f611d
Ritik <ritikpal227@gmail>
2026-07-22 18:39:36 +0530

    Update Admin Home Page

--- client/src/pages/Admin/AdminHome.jsx
+++ client/src/pages/Admin/AdminHome.jsx
@@ -7,14 +7,9 @@ const AdminHome = () => {
   return (
     <>
       <Section title={"Admin Page"} />
-      <div className="container-fluid">
-        <div className="row m-2">
-         <div className="flex flex-col lg:flex-row">
-  <AdminAsideSection />
-  <div className="flex-1 w-full px-4 py-4 lg:px-6">
-    {/* baaki content yahan */}
-  </div>
-</div>
+      <div className="flex flex-col lg:flex-row">
+        <AdminAsideSection />
+        <div className="flex-1 w-full px-4 py-4 lg:px-6">
         </div>
       </div>
     </>

27035f0d277e3a7ef68a03792def9469023faf94
Ritik <ritikpal227@gmail>
2026-07-23 23:37:51 +0530

    Update Admin

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const ContactController = require('../controllers/contactController');
 const { authenticate } = require('../middleware/auth');
-const { isAdmin } = require('../middleware/isAdmin');
+// const { isAdmin } = require('../middleware/isAdmin');
 
 router.post('/', ContactController.postContact);
-router.get('/', authenticate, isAdmin, ContactController.getContact);
+router.get('/', authenticate, ContactController.getContact);
 
 
 module.exports = router;

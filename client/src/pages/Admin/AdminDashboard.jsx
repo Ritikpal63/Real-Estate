4847842ce028641feb864399d03226ff724317b1
Ritik <ritikpal227@gmail>
2026-07-23 23:36:17 +0530

    Update Admin

--- client/src/pages/Admin/AdminDashboard.jsx
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -7,7 +7,7 @@ const AdminDashboardStats = () => {
   const getDetails = async () =>{
     try {
       const newsCount = await axiosInstance.get('/news/allnews')
-      const contactCount = await axiosInstance.get('/contact/all')
+      const contactCount = await axiosInstance.get('/contact')
       setCountNews(newsCount.data.pagination.total)
       setCountContact(contactCount.data.pagination.total)
     } catch (error) {

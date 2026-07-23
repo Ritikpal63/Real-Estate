a18dc9221daffba69ff98118cf7338d7ae24c674
Ritik <ritikpal227@gmail>
2026-07-23 21:09:10 +0530

    Update Admin

--- client/src/pages/Admin/AdminDashboard.jsx
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosInstance from '../../utils/axiosConfig'
 
 const AdminDashboardStats = () => {
+  const [countNews, setCountNews] = useState()
+  const getDetails = async () =>{
+    try {
+      const res = await axiosInstance.get('/news/allnews')
+      setCountNews(res.data.total)
+    } catch (error) {
+      console.log("Dashboard: ", error)
+    }
+  }
+  useEffect(()=>{
+    getDetails()
+  },[])
   return (
     <div>AdminDashboardStats</div>
   )

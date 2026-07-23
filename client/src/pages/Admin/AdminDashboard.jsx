60790c88c30388d4f8f13015f7ef60b2bb76e5c6
Ritik <ritikpal227@gmail>
2026-07-23 22:38:49 +0530

    Update Admin

--- client/src/pages/Admin/AdminDashboard.jsx
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -3,10 +3,13 @@ import axiosInstance from '../../utils/axiosConfig'
 
 const AdminDashboardStats = () => {
   const [countNews, setCountNews] = useState()
+  const [countContact, setCountContact] = useState()
   const getDetails = async () =>{
     try {
       const newsCount = await axiosInstance.get('/news/allnews')
+      const contactCount = await axiosInstance.get('/contact')
       setCountNews(newsCount.data.pagination.total)
+      setCountContact(contactCount.data.pagination.total)
     } catch (error) {
       console.log("Dashboard: ", error)
     }
@@ -15,7 +18,10 @@ const AdminDashboardStats = () => {
     getDetails()
   },[])
   return (
+    <>
     <div>Total News:  {countNews}</div>
+    <div>Total Contact: {countContact}</div>
+    </>
   )
 }
 

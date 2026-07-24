96d76c5da591737ea6c6a100884c109714200339
Ritik <ritikpal227@gmail>
2026-07-24 13:53:32 +0530

    Update Property

--- client/src/pages/Admin/Property/AdminViewProperty.jsx
+++ client/src/pages/Admin/Property/AdminViewProperty.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Section from '../../../components/Section'
+import AdminAsideSection from '../AdminAsideSection'
 
 const AdminViewProperty = () => {
   return (
-    <div>AdminViewProperty</div>
+    <>
+    <Section title={"View Property"}/>
+    <div className="flex flex-col lg:flex-row">
+        <AdminAsideSection />
+        <div className="flex-1 w-full min-w-0 bg-gray-50">
+        </div>
+      </div>
+    </>
   )
 }
 

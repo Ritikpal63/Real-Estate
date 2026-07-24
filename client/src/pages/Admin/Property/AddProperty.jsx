470a8797643a67034b75d70905b8401c86770e64
Ritik <ritikpal227@gmail>
2026-07-24 14:13:23 +0530

    Update Property

--- client/src/pages/Admin/Property/AddProperty.jsx
+++ client/src/pages/Admin/Property/AddProperty.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import ImageUploader from "./ImageUploader";
+import axiosInstance from "../../../utils/axiosConfig";
+import { useEffect } from "react";
 
 export default function AddProperty() {
   const [form, setForm] = useState({
@@ -18,64 +20,75 @@ export default function AddProperty() {
   return (
     <div className="max-w-6xl mx-auto px-3 sm:px-6 py-4 sm:py-8">
       <div className="flex flex-col lg:flex-col sm:flex-row sm:items-center sm:justify-between gap-3 mb-6">
-        
-      
-      <h2 className="text-2xl font-semibold">Add Key Property Information</h2>
+        <h2 className="text-2xl font-semibold">Add Key Property Information</h2>
 
-      <p className="text-gray-500 mt-1 mb-8">
-        Describe your real estate asset.
-      </p>
+        <p className="text-gray-500 mt-1 mb-8">
+          Describe your real estate asset.
+        </p>
 
-      <div className="space-y-5">
-        <input
-          placeholder="Property Name"
-          className="w-full bg-gray-100 rounded-xl px-4 py-3 mb-3 outline-none"
-        />
+        <div className="space-y-5">
+          <input
+            placeholder="Property Name"
+            className="w-full bg-gray-100 rounded-xl px-4 py-3 mb-3 outline-none"
+          />
 
-        <input
-          placeholder="Location"
-          className="w-full bg-gray-100 rounded-xl px-4 mb-3 py-3 outline-none"
-        />
+          <input
+            placeholder="Location"
+            className="w-full bg-gray-100 rounded-xl px-4 mb-3 py-3 outline-none"
+          />
 
-        <select className="w-full bg-gray-100 rounded-xl mb-3 px-4 py-3 outline-none">
-          <option>Select Property Type</option>
-          <option>Villa</option>
-          <option>Apartment</option>
-          <option>Office</option>
-          <option>Land</option>
-        </select>
+          <select className="w-full bg-gray-100 rounded-xl mb-3 px-4 py-3 outline-none">
+            <option>Select Property Type</option>
+            <option>Villa</option>
+            <option>Apartment</option>
+            <option>Office</option>
+            <option>Land</option>
+          </select>
 
-        <input
-          placeholder="Amenities"
-          className="w-full bg-gray-100 rounded-xl mb-3 px-4 py-3 outline-none"
-        />
+          <input
+            placeholder="Amenities"
+            className="w-full bg-gray-100 rounded-xl mb-3 px-4 py-3 outline-none"
+          />
 
-       <div className="grid grid-cols-2 gap-4">
-<div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-  <input type="number" placeholder="Bedrooms" className="bg-gray-100 rounded-xl px-4 py-3 w-full" />
-  <input type="number" placeholder="Bathrooms" className="bg-gray-100 rounded-xl px-4 py-3 w-full" />
-</div>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              <input
+                type="number"
+                placeholder="Bedrooms"
+                className="bg-gray-100 rounded-xl px-4 py-3 w-full"
+              />
+              <input
+                type="number"
+                placeholder="Bathrooms"
+                className="bg-gray-100 rounded-xl px-4 py-3 w-full"
+              />
+            </div>
 
-<div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-  <input placeholder="Size (SQM)" className="bg-gray-100 rounded-xl px-4 py-3 w-full" />
-  <input placeholder="Year Built" className="bg-gray-100 rounded-xl px-4 py-3 w-full" />
-</div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              <input
+                placeholder="Size (SQM)"
+                className="bg-gray-100 rounded-xl px-4 py-3 w-full"
+              />
+              <input
+                placeholder="Year Built"
+                className="bg-gray-100 rounded-xl px-4 py-3 w-full"
+              />
+            </div>
 
+            <textarea
+              rows={5}
+              placeholder="About Property"
+              className="w-full bg-gray-100 rounded-xl px-4 py-3"
+            />
 
-        <textarea
-          rows={5}
-          placeholder="About Property"
-          className="w-full bg-gray-100 rounded-xl px-4 py-3"
-        />
+            <ImageUploader />
 
-        <ImageUploader />
-
-        <button className="w-full bg-[#374256] text-white rounded-lg py-3">
-          Continue
-        </button>
+            <button className="w-full bg-[#374256] text-white rounded-lg py-3">
+              Continue
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     </div>
   );
 }
--- client/src/pages/Admin/Property/AdminViewProperty.jsx
+++ client/src/pages/Admin/Property/AdminViewProperty.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import Section from '../../../components/Section'
-import AdminAsideSection from '../AdminAsideSection'
+import React from "react";
+import Section from "../../../components/Section";
+import AdminAsideSection from "../AdminAsideSection";
 
 const AdminViewProperty = () => {
+  const [property, setProperty] = useState([]);
+  const getproperty = async () => {
+    try {
+      const res = await axiosInstance.get("/property");
+      setProperty(res.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getproperty();
+  }, []);
+
   return (
     <>
-    <Section title={"View Property"}/>
-    <div className="flex flex-col lg:flex-row">
+      <Section title={"View Property"} />
+      <div className="flex flex-col lg:flex-row">
         <AdminAsideSection />
         <div className="flex-1 w-full min-w-0 bg-gray-50">
+            {property.map((item)=>{
+                return <div key={item.id}>
+                    {item.title}
+                </div>
+            })}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default AdminViewProperty
+export default AdminViewProperty;

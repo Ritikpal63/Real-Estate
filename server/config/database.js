83f96e0ee2f32f546d92ba0172079aab74fdd133
Ritikpal_63 <ritikpal227@gmail.com>
2026-08-17 13:12:02 +0530

    update

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,18 @@
 // config/database.js
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'real_estate',
+  database: process.env.DB_NAME || "real_estate",
   waitForConnections: true,
   connectionLimit: 3,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    minVersion: "TLSv1.2",
+  },
 });
 
 module.exports = pool;

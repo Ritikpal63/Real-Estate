d7092f26b54abe3d3b58856ee3a1cb185fa105e2
Ritikpal_63 <ritikpal227@gmail.com>
2026-08-18 16:32:06 +0530

    update features post section

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -321,7 +321,7 @@ const Home = () => {
         id="home"
         className="
           relative
-          min-h-[80vh]
+          banner
           flex
           items-center
           justify-center

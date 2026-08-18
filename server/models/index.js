474824639a50b6760d72b8b562baa63925e47877
Ritikpal_63 <ritikpal227@gmail.com>
2026-08-18 14:35:12 +0530

    update features post section

--- server/models/index.js
+++
@@ -1,6 +0,0 @@
-const sequelize = require("../config/sequelize");
-const Contact = require("./Contact");
-const News = require("./News");
-// const User = require("./User");
-
-module.exports = { sequelize, Contact, News };

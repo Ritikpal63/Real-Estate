d1c055b7b7e54c0ad660407d47d6601a78937cd3
Ritikpal_63 <ritikpal227@gmail.com>
2026-08-18 13:27:01 +0530

    update bg-color

--- client/src/services/serviceApi.js
+++ client/src/services/serviceApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "real-estate-sand-five.vercel.app/api/services",
+  baseURL: "real-estate-sand-five.vercel.app/api/",
 });
 
 export const getServices = () => API.get("/");

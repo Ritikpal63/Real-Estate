441f9c9bbf09fd0723a0b504b0c1f354954a5c53
Ritikpal_63 <ritikpal227@gmail.com>
2026-08-19 12:11:44 +0530

    Update Port

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 // server/server.js — used only for local development
 const app = require('./app');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });

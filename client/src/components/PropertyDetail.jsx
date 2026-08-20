f988e084a0c08293e66e2742456f7522c4497fcb
Ritikpal_63 <ritikpal227@gmail.com>
2026-08-20 16:36:54 +0530

    add company image link

--- client/src/components/PropertyDetail.jsx
+++ client/src/components/PropertyDetail.jsx
@@ -113,7 +113,7 @@ const PropertyDetail = () => {
             <div className="property_single_details_slide">
               <img
                 src={property.image}
-                className="img-fluid h-[400px] w-full"
+                className="h-[400px] w-full"
                 alt={property.title}
                 onError={(e) => { e.currentTarget.src = FALLBACK_IMG; }}
               />

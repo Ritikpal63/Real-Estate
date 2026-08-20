1c2529bec5ec356b5242af73c70bed6563e1e364
Ritikpal_63 <ritikpal227@gmail.com>
2026-08-20 16:07:39 +0530

    porperty size

--- client/src/components/AllProperty.jsx
+++ client/src/components/AllProperty.jsx
@@ -104,26 +104,20 @@ const AllProperty = () => {
                         (window.location.href = `/property/${p.id}`)
                       }
                     >
-                      <div className="single_property rounded-[30px] overflow-hidden">
-                        {/* Fixed Image Container */}
-                        <div className="w-full h-[250px] overflow-hidden">
-                          <img
-                            src={p.image}
-                            alt={p.title}
-                            className="w-auto h-full object-cover object-center"
-                          />
-                        </div>
-
+                      <div className="single_property w-[350px]">
+                        <img
+                          src={p.image}
+                          className="img-fluid"
+                          alt={p.title}
+                        />
                         <div className="single_property_content">
                           <h4>
                             <Link to={`/property/${p.id}`}>{p.title}</Link>
                           </h4>
-
                           <p>{p.location}</p>
                         </div>
-
                         <div className="single_property_price">
-                          {p.location}
+                          {p.location}{" "}
                           <span>${Number(p.price || 0).toLocaleString()}</span>
                         </div>
                       </div>
